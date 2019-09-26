6ac475c8b7e45999b3c49464fa35e4252f97d68f
Piet Geursen <pietgeursen@gmail.com>
2019-09-27 00:12:49 +1200

    Slightly broken but trying to move to material

--- app/src/main/java/nz/scuttlebutt/android_go/activities/MainActivity.kt
+++ app/src/main/java/nz/scuttlebutt/android_go/activities/MainActivity.kt
@@ -17,6 +17,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         //val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         setContentView(R.layout.activity_main)
 
