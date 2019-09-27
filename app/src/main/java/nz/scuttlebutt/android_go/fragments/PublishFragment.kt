04030011ed6173f47a1b9d164c4e4322d2e1d381
Piet Geursen <pietgeursen@gmail.com>
2019-09-27 22:05:00 +1200

    Messing with autocomplete. Adds splits of apks by abi.

--- app/src/main/java/nz/scuttlebutt/android_go/fragments/PublishFragment.kt
+++ app/src/main/java/nz/scuttlebutt/android_go/fragments/PublishFragment.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ArrayAdapter
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import nz.scuttlebutt.android_go.R
+import nz.scuttlebutt.android_go.databinding.FragmentPublishBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -18,10 +20,25 @@ class PublishFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return TextView(activity).apply {
-            setText(R.string.hello_blank_fragment)
-        }
+        //return inflater.inflate(R.layout.fragment_publish, container, false)
+        val binding = DataBindingUtil.inflate<FragmentPublishBinding>(
+            inflater,
+            R.layout.fragment_publish,
+            container,
+            false
+        )
+
+
+        val adapter = ArrayAdapter<String>(
+            this.context!!,
+            android.R.layout.simple_dropdown_item_1line, COUNTRIES
+        )
+        binding.autoCompleteTextView.setAdapter(adapter)
+
+        return binding.root
     }
+
+    private val COUNTRIES = arrayOf("Belgium", "France", "Italy", "Germany", "Spain")
 
 
 }
